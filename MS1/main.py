ea3d85308539346ea0ca33049f02008906851344
Pavel Trninkov <58889027+paveltrninkov@users.noreply.github.com>
2025-11-03 10:21:40 -0700

    did some stuff

--- MS1/main.py
+++ MS1/main.py
@@ -6,7 +6,24 @@
 '''
 
 def main() -> None:
+    count = 0
+    while True:
+        count += 1
+        selection = gather_user_input()
+        break
     return None
 
 def gather_user_input() -> int:
+    return None
+
+def decision(selection):
+    return None
+
+def move(source, target, disks):
+    return None
+
+def win_check(target, disks):
+    return None
+
+def print_status():
     return None
