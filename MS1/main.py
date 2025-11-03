de5b3964e1bed0595a46869e5b3e6b468995a755
Pavel Trninkov <58889027+paveltrninkov@users.noreply.github.com>
2025-11-03 09:57:31 -0700

    Initial commit

---
+++ MS1/main.py
@@ -0,0 +1,12 @@
+'''
+|--------------------------------------------------------------------|
+|Names: Pavel Trninkov, Alex Bachynsky
+|Project: MS1
+|--------------------------------------------------------------------|
+'''
+
+def main() -> None:
+    return None
+
+def gather_user_input() -> int:
+    return None
