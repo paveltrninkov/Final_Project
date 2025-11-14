7447a891e950301ba87cf1277e775c96b3188e7f
paveltrninkov <58889027+paveltrninkov@users.noreply.github.com>
2025-11-14 10:06:35 -0700

    Fixed more logic and edge cases

--- MS1/main.py
+++ MS1/main.py
@@ -12,27 +12,33 @@ def main() -> None:
     Returns: None.
     '''
     while True:
-        selection = gather_user_input(["Enter 1 to Start a new game and 2 to Resume a saved game: "])[0]
-        if selection == 1:
+        selection = gather_user_input(["Enter 1 to Start a new game and 2 to Resume a saved game: "], 2)
+        if selection[0] == 1:
             start_new_game()
         else:
             break
     return None
 
-def gather_user_input(user_questions) -> list:
+def gather_user_input(user_questions:list, max_limit:int) -> list:
     ''''
     Purpose: Helper function to gather user inputs.
-    Parameters: A list of questions.
+    Parameters: A list of questions and a limit to the max acceptable int.
     Returns: A list of user inputs.
     '''
     answers = []
-    for question in user_questions:
-        user_input = input(question)
-        if validate_input(user_input):
-            answers.append(int(user_input))
-        else:
-            print("Error: Invalid answer.")
-    print(answers)
+    question = 0
+    while True:
+        if question > len(user_questions) - 1:
+            break
+        user_input = input(user_questions[question])
+        if validate_input(user_input) != True:
+            print("Error: Invalid input.")
+            continue
+        if int(user_input) > max_limit:
+            print("Error: Invalid input.")
+            continue
+        answers.append(int(user_input))
+        question+=1
     return answers
 
 def validate_input(user_input:str) -> bool:
